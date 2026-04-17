43bbd940102acd365df6f53d6fb62b3489ab90d8
Brandon Khumalo <brandonkhumz40@gmail.com>
2026-04-17 12:33:03 +0200

    fix syntax error in TeacherPerformance.jsx

--- src/pages/teacher/TeacherPerformance.jsx
+++ src/pages/teacher/TeacherPerformance.jsx
@@ -63,7 +63,7 @@ export default function TeacherPerformance() {
   const getGradeColor = (grade) => {
     switch (grade) {
       case "A": return "bg-green-100 text-green-800 border-green-300";
-      case "B": return \"bg-blue-100 text-blue-800 border-blue-300\";
+      case "B": return "bg-blue-100 text-blue-800 border-blue-300";
       case "C": return "bg-yellow-100 text-yellow-800 border-yellow-300";
       case "D": return "bg-orange-100 text-orange-800 border-orange-300";
       case "E": return "bg-red-100 text-red-800 border-red-300";
