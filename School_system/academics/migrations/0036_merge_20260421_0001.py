316f8c385478e3e75dc7a4041bab1f441f8264b0
Brandon Khumalo <brandonkhumz40@gmail.com>
2026-04-21 11:23:13 +0200

    fixed failed tests

---
+++ School_system/academics/migrations/0036_merge_20260421_0001.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('academics', '0035_student_transfer'),
+        ('academics', '0035_reportcardrelease_access_scope_and_generation'),
+    ]
+
+    operations = []
+
